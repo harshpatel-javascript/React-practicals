cd5910629714d4ef930d945b7aeddba77429f2b4
harsh-javascript <harsh.patel@simformsolutions.com>
2022-03-09 16:06:30 +0530

    some commnets

---
+++ to-do-list copy/src/Components/ListItems.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+import taskIcon from "../Images/task-completed-icon.svg";
+function ListItems({ todo }) {
+  const [checked, setChecked] = useState(localStorage.getItem(todo.id));
+  // state handling as per the input:checkbox
+  const checkedHandler = (e) => {
+    const isNotChecked = e.target.checked;
+    if (isNotChecked) {
+      setChecked(true);
+      localStorage.setItem(todo.id, true);
+    } else {
+      setChecked(false);
+      localStorage.removeItem(todo.id);
+    }
+  };
+  return (
+    <div className={checked ? "list completed" : "list not-completed"}>
+      <p>{todo.title}</p>
+      <input
+        className="list-checkbox"
+        type="checkbox"
+        onChange={checkedHandler}
+      />
+      <img
+        className={checked ? "task-icon show" : "task-icon hidden"}
+        src={taskIcon}
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default ListItems;
---
+++ to-do-list copy/src/Components/TodoList.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import ListItems from "./ListItems";
+import "./TodoList.css";
+
+function TodoList({ todos }) {
+  return (
+    <div className="list-container">
+      {todos.length < 1 ? (
+        <div className="empty-statement">No Todo List for Today.</div>
+      ) : (
+        todos.map((todo) => <ListItems key={todo.id} todo={todo} />)
+      )}
+    </div>
+  );
+}
+export default TodoList;
