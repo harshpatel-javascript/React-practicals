38595bd9077d5b3bb17813a366019c80edce9880
harsh-javascript <harsh.patel@simformsolutions.com>
2022-03-25 14:57:41 +0530

    final commit

---
+++ practical-6/src/components/UserContainer.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import "../css/UserContainer.css";
+import Card from "./Card";
+import UserList from "./UserList";
+import Header from "./Header";
+import Pagination from "react-bootstrap/Pagination";
+import { fetchUsers, handlePage } from "../redux/action";
+
+function UserContainer() {
+  const { loading, cardData, fetchedData, error, activePage } = useSelector(
+    (state) => state
+  );
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUsers(activePage));
+  }, [dispatch, activePage]);
+  //getting the detail for card as hovering
+
+  return loading ? (
+    <h2>Loading</h2>
+  ) : error ? (
+    <h2>{error}</h2>
+  ) : (
+    <>
+      <div className="user-container">
+        <Header />
+        {fetchedData.map((data) => (
+          <UserList data={data} key={data.id} />
+        ))}
+        <Pagination>
+          <Pagination.Prev />
+          <Pagination.Item onClick={() => dispatch(handlePage(1))}>
+            1
+          </Pagination.Item>
+          <Pagination.Item onClick={() => dispatch(handlePage(2))}>
+            2
+          </Pagination.Item>
+          <Pagination.Last />
+        </Pagination>
+      </div>
+      {/* card data */}
+      {cardData && <Card data={cardData} />}
+    </>
+  );
+}
+
+export default UserContainer;
---
+++ practical-6/src/redux/reducer.js
@@ -0,0 +1,76 @@
+import {
+  PASS_DATA_TO_CARD,
+  TOGGLE_STATUS,
+  MOUSE_LEAVE,
+  DELETE_USER,
+  CHANGE_PAGE,
+  FETCH_USERS_REQUEST,
+  FETCH_USERS_SUCCESS,
+  FETCH_USERS_FAILURE,
+} from "./types";
+
+const initialState = {
+  loading: false,
+  cardData: false,
+  error: "",
+  fetchedData: [],
+  activePage: 1,
+};
+
+const reducer = (state = initialState, action) => {
+  console.log();
+  switch (action.type) {
+    case FETCH_USERS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case FETCH_USERS_SUCCESS:
+      return {
+        ...state,
+        fetchedData: action.payload.data,
+      };
+    case FETCH_USERS_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case PASS_DATA_TO_CARD:
+      return {
+        ...state,
+        cardData: action.payload,
+      };
+    case CHANGE_PAGE:
+      return {
+        ...state,
+        activePage: action.payload.activePage,
+      };
+    case TOGGLE_STATUS:
+      return {
+        ...state,
+        fetchedData: state.fetchedData.map((data) => {
+          if (data.id === action.payload.id) {
+            return {
+              ...data,
+              status: action.payload.status,
+            };
+          }
+          return data;
+        }),
+      };
+    case MOUSE_LEAVE:
+      return {
+        ...state,
+      };
+    case DELETE_USER:
+      const { id } = action.payload;
+      return {
+        ...state,
+        fetchedData: state.fetchedData.filter((data) => data.id !== id, 1),
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
