cfde3a4b933d0d73cdb61bfa3168e9e2346fb156
harshpatel-javascript <97085903+harshpatel-javascript@users.noreply.github.com>
2022-03-25 12:48:52 +0530

    Delete user-list-react-app directory

--- user-list-react-app/src/redux/types.js
+++
@@ -1,4 +0,0 @@
-export const PASS_DATA_TO_CARD = "PASS_DATA_TO_CARD";
-export const DELETE_USER = "DELETE_USER";
-export const TOGGLE_STATUS = "TOGGLE_STATUS";
-export const MOUSE_LEAVE = "MOUSE_LEAVE";
