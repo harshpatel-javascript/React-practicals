74202c2be531862e9dce172b7ea00660cc59b47f
harsh-javascript <harsh.patel@simformsolutions.com>
2022-03-28 17:05:23 +0530

    remove unused code

--- practical-7/src/redux/action.js
+++ practical-7/src/redux/action.js
@@ -62,7 +62,7 @@ export const deleteUser = (id) => {
   };
 };
 
-export const handlePage = (page, data) => {
+export const handlePage = (page) => {
   return {
     type: CHANGE_PAGE,
     payload: {
